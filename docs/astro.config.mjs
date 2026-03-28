f9b4f7842664a1cb6949ee05b5003648a97751fc
UnoxyRich <justin.yonardo@outlook.com>
2026-03-28 11:23:42 +0800

    Update astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
-  site: 'https://unoxyrich.github.io',
-  base: '/Lingo-Stream',
+  site: 'https://404-waifu-not-found.github.io',
+  base: '/HackMITChina2026',
   integrations: [tailwind({ applyBaseStyles: false })],
   output: 'static',
 })
